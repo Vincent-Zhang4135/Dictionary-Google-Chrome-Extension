c0fdfe0c6ae0dca98df2fe4f85a7a475309bbfef
Vincent-Zhang4135 <65748334+Vincent-Zhang4135@users.noreply.github.com>
2021-09-02 16:38:16 -0400

    developed a presentable hard-coded representation of the UI for the dictionary

---
+++ index.js
@@ -0,0 +1,13 @@
+dict_key = "16450aa8-91b4-40f8-a029-923295ab74d2"
+thes_key = "c1c55072-9ee8-4cf8-b66a-f688ef151764"
+
+let word = "voluminous"
+let definitions = ["having or marked by great volume"]
+
+function render() {
+
+}
+
+function renderWord() {
+
+}
