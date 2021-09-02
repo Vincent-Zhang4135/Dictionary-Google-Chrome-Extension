b7a28d5450576a1b60f4be977f78930295cab2ec
Vincent-Zhang4135 <65748334+Vincent-Zhang4135@users.noreply.github.com>
2021-09-02 17:21:50 -0400

    added rendering functionality of the word and definitions

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-dict_key = "16450aa8-91b4-40f8-a029-923295ab74d2"
-thes_key = "c1c55072-9ee8-4cf8-b66a-f688ef151764"
+const dict_key = "16450aa8-91b4-40f8-a029-923295ab74d2"
+const thes_key = "c1c55072-9ee8-4cf8-b66a-f688ef151764"
 
-let word = "voluminous"
-let definitions = ["having or marked by great volume"]
+const wordEl = document.getElementById("word")
+const testEl = document.getElementById("testing")
+const definitionsEl = document.getElementById("definitions")
+let word = "Voluminous"
+let definitions = ["having or marked by great volume", "having great volume", "another definition", "last one?"]
+let partsOfSpeech = ["n", "v", "adj", "p"]
+
+testEl.addEventListener("click", function() {
+    render()
+})
 
 function render() {
+    renderWord()
+    renderDefinitions()
+}
 
+function clear() {
+    wordEl.innerHTML = ""
+    definitionsEl.innerHTML = ""
 }
 
 function renderWord() {
-
+    wordEl.innerHTML = word
 }
+
+function renderDefinitions() {
+    for (let i = 0; i < 3; i++) {
+        if (definitions[i]) {
+            definitionsEl.innerHTML +=
+            `<li>
+                <p>
+                    <span class="parts-of-speech">${partsOfSpeech[i]}.</span>
+                    <span class="definition">${definitions[i]}</span>
+                </p>
+            </li>
+            `
+        }
+    }
+}
+
