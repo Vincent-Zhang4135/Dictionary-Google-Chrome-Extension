8f45a4c3939743ca85603dc1155d51991d6b55d4
Vincent-Zhang4135 <65748334+Vincent-Zhang4135@users.noreply.github.com>
2021-09-03 12:41:28 -0400

    fixed bug issue relating to parts of speech not formatting or rendering correctly

--- index.js
+++ index.js
@@ -3,37 +3,41 @@ const thes_key = "c1c55072-9ee8-4cf8-b66a-f688ef151764"
 
 const wordEl = document.getElementById("word")
 const inputEl = document.getElementById("input-word")
+const posEl = document.getElementById("part-of-speech")
 const definitionsEl = document.getElementById("definitions")
-let word = "Voluminous"
-let definitions = ["having or marked by great volume", "having great volume", "another definition", "last one?"]
-let partsOfSpeech = ["n", "v", "adj", "p"]
+const partsOfSpeech = {
+    "pronoun" : "pn.",
+    "noun" : "n.",
+    "adjective" : "adj.",
+    "verb" : "v.",
+    "adverb" : "adv.",
+    "conjunction" : "conj.",
+    "interjection" : "intj.",
+    "preposition" : "prep."
+}
 
-inputEl.addEventListener("keyup", function(e) {
+let word = ""
+
+inputEl.addEventListener("keyup", async function(e) {
     if (e.key === "Enter") {
         word = inputEl.value
         inputEl.value = ""
         // clear out the previous entry, and then render the new one
         clear()
-        fetchFromDictionaryAPI()
-        render()
+        const dictionary = await fetchFromDictionaryAPI()
+        render(dictionary)
     }
 })
 
 async function fetchFromDictionaryAPI() {
     url = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${dict_key}`
     try {
-        let promise = await fetch(url)
+        let promise = await fetch(url) 
         response = await promise.json()
     } catch (error) {
         console.error(error)
     }
-
-    return response
-}
-
-function render() {
-    renderWord()
-    renderDefinitions()
+    return response[0]
 }
 
 function clear() {
@@ -41,18 +45,25 @@ function clear() {
     definitionsEl.innerHTML = ""
 }
 
-function renderWord() {
-    wordEl.innerHTML = word
+function render(dict) {
+    renderWord(dict)
+    renderDefinitions(dict)
 }
 
-function renderDefinitions() {
+function renderWord(dict) {
+    syllableWord = dict.hwi.hw.replace(/\*/g, "-")
+    partOfSpeech = dict.fl
+    wordEl.innerHTML = syllableWord
+    posEl.innerHTML = partsOfSpeech[partOfSpeech]
+}
+
+function renderDefinitions(dict) {
     for (let i = 0; i < 3; i++) {
-        if (definitions[i]) {
+        if (dict.shortdef[i]) {
             definitionsEl.innerHTML +=
             `<li>
-                <p>
-                    <span class="parts-of-speech">${partsOfSpeech[i]}.</span>
-                    <span class="definition">${definitions[i]}</span>
+                <p class="definition">
+                    <span class="shortdef">${dict.shortdef[i]}</span>
                 </p>
             </li>
             `
